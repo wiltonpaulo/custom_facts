c13af164388cf195c5886c1950229287867898c0
rscudeller <roberto.scudeller@oi.net.br>
2015-10-09 18:20:38 -0300

    add changes from oi.infra

--- lib/facter/roles.rb
+++ lib/facter/roles.rb
@@ -1,11 +1,10 @@
-# ([a-z]+)[0-9]+, i.e. www01 or logger22 have a puppet_role of www or logger
+# ([a-z]+)[0-9]+, i.e. server-01 or logger-22 have a puppet_role of server or logger
 if Facter.value(:hostname) =~ /^([a-z]+)-[0-9]+$/
   Facter.add('puppet_role') do
     setcode do
       $1
     end
   end
-
 # ([a-z]+), i.e. www or logger have a puppet_role of www or logger
 elsif Facter.value(:hostname) =~ /^([a-z]+)$/
   Facter.add('puppet_role') do
@@ -13,7 +12,27 @@ elsif Facter.value(:hostname) =~ /^([a-z]+)$/
       $1
     end
   end
-
+# ([a-z]+[-]+[a-z]+), i.e. www-ddd or logger-ddd have a puppet_role of www-ddd or logger-ddd
+elsif Facter.value(:hostname) =~ /^([a-z]+[-]+[a-z]+)/
+  Facter.add('puppet_role') do
+    setcode do
+      $1
+    end
+  end
+# ([a-z]+[-]+[a-z]+), i.e. www-ddd-eee or logger-ddd-eee have a puppet_role of www-ddd-eee or logger-ddd-eee
+elsif Facter.value(:hostname) =~ /^([a-z]+[-]+[a-z]+[-]+[a-z]+)/
+  Facter.add('puppet_role') do
+    setcode do
+      $1
+    end
+  end
+# ([a-z]+[-]+[a-z]+), i.e. www-dd-ee-ff or logger-dd-ee-ff have a puppet_role of www-dd-ee-ff or logger-dd-ee-ff
+elsif Facter.value(:hostname) =~ /^([a-z]+[-]+[a-z]+[-]+[a-z]+[-]+[a-z]+)/
+  Facter.add('puppet_role') do
+    setcode do
+      $1
+    end
+  end
 # Set to hostname if no patterns match
 else
   Facter.add('puppet_role') do
