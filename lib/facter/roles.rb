4d95032309dc2c27849cbdd3743008a1a8e6992d
rscudeller <roberto.scudeller@oi.net.br>
2015-11-26 16:19:47 -0200

    change order of processing into roles.rb

--- lib/facter/roles.rb
+++ lib/facter/roles.rb
@@ -1,19 +1,5 @@
-# ([a-z]+)[0-9]+, i.e. server-01 or logger-22 have a puppet_role of server or logger
-if Facter.value(:hostname) =~ /^([a-z]+)-[0-9]+$/
-  Facter.add('puppet_role') do
-    setcode do
-      $1
-    end
-  end
-# ([a-z]+), i.e. www or logger have a puppet_role of www or logger
-elsif Facter.value(:hostname) =~ /^([a-z]+)$/
-  Facter.add('puppet_role') do
-    setcode do
-      $1
-    end
-  end
-# ([a-z]+[-]+[a-z]+), i.e. www-ddd or logger-ddd have a puppet_role of www-ddd or logger-ddd
-elsif Facter.value(:hostname) =~ /^([a-z]+[-]+[a-z]+)/
+# ([a-z]+[-]+[a-z]+), i.e. www-dd-ee-ff or logger-dd-ee-ff have a puppet_role of www-dd-ee-ff or logger-dd-ee-ff
+if Facter.value(:hostname) =~ /^([a-z]+[-]+[a-z]+[-]+[a-z]+[-]+[a-z]+)/
   Facter.add('puppet_role') do
     setcode do
       $1
@@ -26,8 +12,22 @@ elsif Facter.value(:hostname) =~ /^([a-z]+[-]+[a-z]+[-]+[a-z]+)/
       $1
     end
   end
-# ([a-z]+[-]+[a-z]+), i.e. www-dd-ee-ff or logger-dd-ee-ff have a puppet_role of www-dd-ee-ff or logger-dd-ee-ff
-elsif Facter.value(:hostname) =~ /^([a-z]+[-]+[a-z]+[-]+[a-z]+[-]+[a-z]+)/
+# ([a-z]+[-]+[a-z]+), i.e. www-ddd or logger-ddd have a puppet_role of www-ddd or logger-ddd
+elsif Facter.value(:hostname) =~ /^([a-z]+[-]+[a-z]+)/
+  Facter.add('puppet_role') do
+    setcode do
+      $1
+    end
+  end
+# ([a-z]+)[0-9]+, i.e. server-01 or logger-22 have a puppet_role of server or logger
+elsif Facter.value(:hostname) =~ /^([a-z]+)-[0-9]+$/
+  Facter.add('puppet_role') do
+    setcode do
+      $1
+    end
+  end
+# ([a-z]+), i.e. www or logger have a puppet_role of www or logger
+elsif Facter.value(:hostname) =~ /^([a-z]+)$/
   Facter.add('puppet_role') do
     setcode do
       $1
